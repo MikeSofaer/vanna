1c9a1a564331efc1ad2b257ad4722dbcaeaa9ac3
Steve Conover <sconover@gmail.com>
2010-09-09 23:51:02 -0700

    Renders json always

--- lib/application_presenter.rb
+++ lib/application_presenter.rb
@@ -1,29 +1,38 @@
 class ApplicationPresenter < ActionController::Metal
   abstract!
-#  def formats; [:html, :json]; end
-  include ActionController::MimeResponds  
-  respond_to :html, :json
-
-  def formats=(rspec_wants_this);end
-  def url_options;{};end
-end
-#
-=begin
-class ApplicationPresenter < ActionController::Metal
   include ActionController::MimeResponds
-  include ActionView::Context
-  include AbstractController::Rendering
-  abstract!
-
-  append_view_path "views"
-
+  
+  #These are just here to stop errors, they do nothing
+  respond_to :html, :json
+  def freeze_formats(f); end
+  def formats; [:html, :json]; end
+  #include ActionView::Context
   def lookup_context
     self
   end
- 
   def controller
     self
   end
+
+end
+module ActionController
+  class Responder
+    protected
+  def default_render
+    controller.response_body = request.format.symbol == :json ?
+      options.to_json : options.to_json
+     # _render_template_from_controller(
+     #   JsonTemplateRenderer.new, ActionPresenter::DEFAULT_LAYOUT, :dictionary => json)
+  end
+  end
+end
+=begin
+class ApplicationPresenter < ActionController::Metal
+  include AbstractController::Rendering
+
+  append_view_path "views"
+
+ 
  
   DEFAULT_LAYOUT = Object.new.tap {|l| def l.render(*) JsonTemplateRenderer.tag + 
     "<body><div id=main></div></body>"+ yield end }
@@ -34,11 +43,8 @@ class ApplicationPresenter < ActionController::Metal
     layout.render(self, options) { ret }
   end
   
-  respond_to :html, :json
   
   
-  def freeze_formats(f); end
   
-  def formats; [:html, :json]; end
 end
 =end
