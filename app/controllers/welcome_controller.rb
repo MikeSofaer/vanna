43a1cb61cc56052a5aae0cc2d1acfafdb24b7e1d
MikeSofaer <github@mike.sofaer.net>
2010-09-11 17:39:26 -0700

    More in the readme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,6 @@
 require 'lib/application_presenter'
 class WelcomeController < ApplicationPresenter
-
-def index
-  @view_paths =  ["app/views"]
-  respond_with :text => "hello"
-end
-
+  def index
+    respond_with :text => "hello"
+  end
 end
