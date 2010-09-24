f14188f9b18f65507f067b9a9b3fb65bf11e2b77
Michael Sofaer <msofaer@pivotallabs.com>
2010-09-24 00:01:10 -0700

    Removing more Rails files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ActionPresenter::Application.config.secret_token = '8b9708cd5d4a96f541e04326467fc9c4b0c0a44c4d77b19632db2145fd1cf686d739f7d34a3ecce551c53dc8d31730e816cd332b3eececd95cc539b52e1aadde'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-ActionPresenter::Application.config.session_store :cookie_store, :key => '_action_presenter_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionPresenter::Application.config.session_store :active_record_store
