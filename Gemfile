9879a33e660874b0744ee49de14784e6bd254466
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2011-10-18 07:41:07 +0200

    better gem description, still not all tests are passing

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source 'http://rubygems.org'
 
 gemspec
+gem "turn", "~> 0.8.3"
+gem "minitest", "~> 2.6.1"
+gem 'wrong'
+gem 'test-unit'
+gem 'rack-test'
+gem 'actionpack'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,17 @@
 ENV["RAILS_ENV"] = "test"
 $LOAD_PATH.unshift './test'
 $LOAD_PATH.unshift './test/app/controllers'
+require 'rubygems'
+gem 'minitest' # ensures you're using the gem, and not the built in MT
+require 'minitest/autorun'
+gem 'test-unit'
+require 'test/unit'
+require 'wrong/adapters/test_unit'
+
 require 'config/application'
 require 'rails/test_help'
 require 'json'
 require 'wrong'
-require 'wrong/adapters/test_unit'
 require "wrong/message/string_comparison"
 require "rack/test"
 Wrong.config[:color] = true
